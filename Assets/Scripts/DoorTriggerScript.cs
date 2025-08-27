6335119131a31175399c6a59c3a11b0cbd180070
JeanFrancoisRetief <2458318@students.wits.ac.za>
2025-08-27 11:35:44 +0200

    Doorway functionality

---
+++ Assets/Scripts/DoorTriggerScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTriggerScript : MonoBehaviour
+{
+    public GameObject LoadingPanel;
+
+    private void Awake()
+    {
+        if (LoadingPanel != null)
+            LoadingPanel.SetActive(false);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(LoadingPanel != null)
+            LoadingPanel.SetActive(true);
+
+        if(other.tag == "DoorOriginCentre")
+        {
+            SceneManager.LoadScene("Museum");
+        }
+        else if (other.tag == "DoorDigitalArts")
+        {
+            SceneManager.LoadScene("");
+        }
+        else if (other.tag == "DoorFineArts")
+        {
+            SceneManager.LoadScene("");
+        }
+        else if (other.tag == "DoorAnthropology")
+        {
+            SceneManager.LoadScene("");
+        }
+        else if (other.tag == "DoorAdler")
+        {
+            SceneManager.LoadScene("");
+        }
+        else if (other.tag == "DoorLifeSciences")
+        {
+            SceneManager.LoadScene("");
+        }
+        else if (other.tag == "DoorExit")
+        {
+            SceneManager.LoadScene("MainHall");
+        }
+
+    }
+}
